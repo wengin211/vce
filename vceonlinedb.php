c152bb8c2c743f4a6aec19205b8ffe29b95b8eb0
wengin211 <37298008+wengin211@users.noreply.github.com>
2018-06-08 14:43:27 +0800

    Update vceonlinedb.php

--- vceonlinedb.php
+++ vceonlinedb.php
@@ -92,8 +92,8 @@ function createadiv(ans){
 	define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
 	define('DB_NAME', getenv('OPENSHIFT_APP_NAME'));
 
-	$mysqlCon = mysqli_connect(DB_HOST, DB_USER, DB_PASS, "", DB_PORT) or die("Error: " . mysqli_error($mysqlCon));
-	mysqli_select_db($mysqlCon, DB_NAME) or die("Error: " . mysqli_error($mysqlCon));
+	$mysqlCon = mysqli_connect(getenv('OPENSHIFT_MYSQL_DB_HOST'), getenv('OPENSHIFT_MYSQL_DB_USERNAME'), getenv('OPENSHIFT_MYSQL_DB_PASSWORD'), "", getenv('OPENSHIFT_MYSQL_DB_PORT')) or die("Error: " . mysqli_error($mysqlCon));
+	mysqli_select_db($mysqlCon, getenv('OPENSHIFT_APP_NAME')) or die("Error: " . mysqli_error($mysqlCon));
 ?>
 <!-- <input type='file' accept='text/plain' onchange='openFile(event)'><br> -->
 <!--<p id='output'></p>-->
