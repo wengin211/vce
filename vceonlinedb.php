ef22ddb16de82d2706bd02abcad05319bfec5010
wengin211 <37298008+wengin211@users.noreply.github.com>
2018-06-08 15:25:22 +0800

    Update vceonlinedb.php

--- vceonlinedb.php
+++ vceonlinedb.php
@@ -91,19 +91,12 @@ $dbport = getenv("OPENSHIFT_EXTMYSQL_DB_PORT");
 $dbuser = getenv("OPENSHIFT_EXTMYSQL_DB_USERNAME");
 $dbpwd = getenv("OPENSHIFT_EXTMYSQL_DB_PASSWORD");
 $dbname = getenv("OPENSHIFT_EXTMYSQL_DB_NAME");
-$connection = mysql_connect($dbhost.":".$dbport, $dbuser, $dbpwd);
+$connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd);
 if (!$connection) {
   echo "Could not connect to database";
 } else {
   echo "Connected to database.<br>";
 }
-$dbconnection = mysql_select_db($dbname);
-$query = "SELECT * from users";
-$rs = mysql_query($query);
-while ($row = mysql_fetch_assoc($rs)) {
-  echo $row['user_id'] . " " . $row['username'] . "\n";
-}
-mysql_close();
 ?>
 <!-- <input type='file' accept='text/plain' onchange='openFile(event)'><br> -->
 <!--<p id='output'></p>-->
